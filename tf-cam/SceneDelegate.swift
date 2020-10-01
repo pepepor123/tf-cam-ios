133bc087292a18095ec8b289d8fe61c66e722c31
hrbysnk <pepepor123@gmail.com>
2020-10-01 22:16:50 +0900

    Remove Main.storyboard

--- tf-cam/SceneDelegate.swift
+++ tf-cam/SceneDelegate.swift
@@ -13,7 +13,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    guard let _ = (scene as? UIWindowScene) else { return }
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    let window = UIWindow(windowScene: windowScene)
+    self.window = window
+    window.rootViewController = ViewController(nibName: nil, bundle: nil)
+    window.makeKeyAndVisible()
   }
 
   func sceneDidDisconnect(_ scene: UIScene) {
