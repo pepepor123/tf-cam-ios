f980f26c2e64e02a144a5d73c19b8fc1ff89efca
hrbysnk <pepepor123@gmail.com>
2020-10-05 18:50:43 +0900

    Make it possible to display camera preview

--- tf-cam/SecondViewController.swift
+++ tf-cam/SecondViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+  var previewView: PreviewView!
+  let button = UIButton(frame: CGRect(x: 40, y: 60, width: 200, height: 60))
+  private lazy var cameraFeedManager = CameraFeedManager(previewView: previewView)
+
   init() {
     super.init(nibName: nil, bundle: nil)
   }
@@ -26,11 +30,71 @@ class SecondViewController: UIViewController {
     view.backgroundColor = UIColor.white
     navigationItem.title = "Second View"
 
-    let button = UIButton(frame: CGRect(x: 40, y: 100, width: 200, height: 60))
+    previewView = PreviewView(frame: self.view.bounds)
+    cameraFeedManager.checkCameraConfigurationAndStartSession()
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+
+    self.previewView.previewLayer.connection?.videoOrientation = .landscapeLeft
+    self.previewView.previewLayer.videoGravity = .resizeAspect
+    view.addSubview(previewView)
+
     button.backgroundColor = UIColor.yellow
     button.setTitle("button", for: .normal)
     button.setTitleColor(.black, for: .normal)
+    button.addTarget(self, action: #selector(btnFunc), for: .touchUpInside)
     self.view.addSubview(button)
+  }
+
+  @objc func btnFunc() {
+    print("Button was pressed.")
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    cameraFeedManager.stopSession()
+  }
+
+}
+
+// MARK: CameraFeedManagerDelegate Methods
+extension SecondViewController: CameraFeedManagerDelegate {
+
+  func didOutput(pixelBuffer: CVPixelBuffer) {
+//    do something
+  }
+
+  func presentVideoConfigurationErrorAlert() {
+    let alertController = UIAlertController(
+      title: "Confirguration Failed",
+      message: "Configuration of camera has failed.",
+      preferredStyle: .alert
+    )
+    
+    let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+    alertController.addAction(okAction)
+
+    present(alertController, animated: true, completion: nil)
+  }
+
+  func presentCameraPermissionsDeniedAlert() {
+    let alertController = UIAlertController(
+      title: "Camera Permissions Denied",
+      message: "Camera permissions have been denied for this app. You can change this by going to Settings",
+      preferredStyle: .alert
+    )
+
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+    let settingsAction = UIAlertAction(title: "Settings", style: .default) { (action) in
+      UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
+    }
+
+    alertController.addAction(cancelAction)
+    alertController.addAction(settingsAction)
+
+    present(alertController, animated: true, completion: nil)
   }
 
 }
