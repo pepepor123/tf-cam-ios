8f53df11c2214cdf19cb09f827ca5d3793a4b254
hrbysnk <pepepor123@gmail.com>
2020-10-05 13:33:14 +0900

    Add PreviewView.swift

---
+++ tf-cam/Camera/PreviewView.swift
@@ -0,0 +1,37 @@
+//
+//  PreviewView.swift
+//  tf-cam
+//
+//  Created by hrbysnk on 2020/10/02.
+//
+
+import UIKit
+import AVFoundation
+
+/**
+ The camera frame is displayed on this view.
+ */
+class PreviewView: UIView {
+
+  var previewLayer: AVCaptureVideoPreviewLayer {
+    guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+      fatalError("Layer expected is of type VideoPreviewLayer")
+    }
+    return layer
+  }
+
+  var session: AVCaptureSession? {
+    get {
+      return previewLayer.session
+    }
+    set {
+      previewLayer.session = newValue
+    }
+  }
+
+  override class var layerClass: AnyClass {
+    return AVCaptureVideoPreviewLayer.self
+  }
+
+}
+
